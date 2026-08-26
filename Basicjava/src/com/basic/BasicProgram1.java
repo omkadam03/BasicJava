7b2f3d1d8a4445bc337010e5b337d84574a3d197
OM KADAM <omkadam.tech@gmail.com>
2026-08-26 16:18:26 +0530

    add basic Java Program

---
+++ Basicjava/src/com/basic/BasicProgram1.java
@@ -0,0 +1,32 @@
+package com.basic;
+
+import java.util.Scanner;
+
+public class BasicProgram1 {
+	
+	public static void main(String args[]) {
+//		String name="OM KADAM";
+//		System.out.println("Name :"+name);//Print your name
+//		
+//		int age=22;
+//		boolean Strudent=true;
+//		String stream ="Computer Science";
+//		System.out.println("Age:"+age);
+//		System.out.println("Stream:"+stream);//Declare and print variables
+//		
+//		int a = 10;
+//		int b= 20;
+//		int add = a+b;
+//		System.out.println("Addition is "+add);//addition of two number
+//		
+//		float radius;
+//		double pi=3.14;
+//		Scanner sc=new Scanner(System.in);
+//		System.out.println("Enter Radius of circle:");
+//		radius=sc.nextFloat();
+//		double area=pi*radius*radius;
+//		System.out.println("Result:"+area);
+				
+	}
+	
+}

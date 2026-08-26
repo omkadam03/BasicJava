923c345aff67d8a6b423e59cc5f4faeea83fb0a3
OM KADAM <omkadam.tech@gmail.com>
2026-08-26 17:07:39 +0530

    Add addition program

---
+++ Basicjava/src/com/basic/BasicProgram2.java
@@ -0,0 +1,16 @@
+package com.basic;
+
+public class BasicProgram2 {
+
+    public static void main(String[] args) {
+
+        int a = 20;
+        int b = 10;
+
+        int addition = a + b;
+
+        System.out.println("First Number  : " + a);
+        System.out.println("Second Number : " + b);
+        System.out.println("Addition      : " + addition);
+    }
+}
